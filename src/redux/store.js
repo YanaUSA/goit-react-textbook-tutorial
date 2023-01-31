d163aada6f1653e2d514543dd6378e7abb1656b3
YanaUSA <usatiukiana@gmail.com>
2023-01-31 22:12:32 +0200

    fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { tasksReducer, filtersReducer } from "./filterSlice";
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 
 export const store = configureStore({
   reducer: {
